13405a46f582134f37afeeec7420e26c1c477ab6
feeeed <76104017+feeeed@users.noreply.github.com>
2024-08-24 18:51:44 +0600

    build v2
    asd

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/PunchGame/",
   plugins: [vue()],
   resolve:{
-    alias:{
+    alias:[
 
-    }
-  }
+    ]
+
+  },
 })
